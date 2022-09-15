91f729404f4ed2c172606cd1412654edc03eae88
Eduardo Castro <eduardocastro.a@hotmail.com>
2022-09-15 19:58:54 -0400

    add react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,45 @@
 import profile from "../images/profile-picture.png";
+import { AiOutlineRight } from 'react-icons/ai'
+import { FaFacebookSquare, FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 
 
 
 export default function Header() {
   return (
-    <header id="home" className="bg-header bg-cover relative bg-cover bg-center bg-no-repeat py-8">
-      <div class="container relative z-30 pt-20 pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48">
-    <div class="flex flex-col items-center justify-center lg:flex-row">
-      <div class="rounded-full border-8 border-primary shadow-xl">
-        <img src={profile} class="h-48 rounded-full sm:h-56" alt="author"/>
+    <header id="home" className="bg-header relative bg-cover bg-center bg-no-repeat py-8">
+      <div className="absolute inset-0 z-20 bg-gradient-to-r from-hero-gradient-from to-hero-gradient-to bg-cover bg-center bg-no-repeat"> </div>
+      <div className="container relative z-30 pt-20 pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48">
+    <div className="flex flex-col items-center justify-center lg:flex-row">
+      <div className="rounded-full border-8 border-primary shadow-xl">
+        <img src={profile} className="h-48 rounded-full sm:h-56" alt="author"/>
       </div>
-      <div class="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
-        <h1 class="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
+      <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
+        <h1 className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
           Hello I'm Eduardo Castro!
         </h1>
-        <div class="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start">
-          <div class="flex items-center justify-center pl-0 sm:justify-start md:pl-1">
-            <p class="font-body text-lg uppercase text-white">Let's connect</p>
-            <div class="hidden sm:block">
-              <i class="bx bx-chevron-right text-3xl text-yellow"></i>
+        <div className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start">
+          <div className="flex items-center justify-center pl-0 sm:justify-start md:pl-1">
+            <p className="font-body text-lg uppercase text-white">Let's connect</p>
+            <div className="hidden sm:block">
+              <AiOutlineRight className="bx bx-chevron-right text-3xl text-yellow"/>
             </div>
           </div>
-          <div class="flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0">
+          <div className="flex items-center justify-center pt-5 pl-2 sm:justify-start sm:pt-0">
             <a href="/">
-              <i class="bx bxl-facebook-square text-2xl text-white hover:text-yellow"></i>
+              <FaFacebookSquare className="text-2xl text-white hover:text-yellow"/>
             </a>
-            <a href="/" class="pl-4">
-              <i class="bx bxl-twitter text-2xl text-white hover:text-yellow"></i>
+            <a href="/" className="pl-4">
+              <FaGithub className="text-2xl text-white hover:text-yellow"/>
             </a>
-            <a href="/" class="pl-4">
-              <i class="bx bxl-dribbble text-2xl text-white hover:text-yellow"></i>
+            <a href="/" className="pl-4">
+              <FaLinkedin className="text-2xl text-white hover:text-yellow"/>
             </a>
-            <a href="/" class="pl-4">
-              <i class="bx bxl-linkedin text-2xl text-white hover:text-yellow"></i>
+            <a href="#contact" className="pl-4">
+              <FaEnvelope className="bx bxl-linkedin text-2xl text-white hover:text-yellow"/>
             </a>
-            <a href="/" class="pl-4">
-              <i class="bx bxl-instagram text-2xl text-white hover:text-yellow"></i>
+            <a href="/" className="pl-4">
+              <i className="bx bxl-instagram text-2xl text-white hover:text-yellow"></i>
             </a>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export default function Navbar() {
   const [selected, setSelected] = useState('Home');
@@ -29,9 +29,9 @@ export default function Navbar() {
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <AiOutlineClose className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <AiOutlineMenu className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
